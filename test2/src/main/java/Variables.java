b6fd9c6b8f434a6eea5bd430d93b1b5a17505180
thidaoC0223g1 <daogiathi231290@gmail.com>
2023-03-22 21:45:58 +0700

    done thuc hanh toan tu

---
+++ test2/src/main/java/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int i = 10;
+        float j = 30.56f;
+        double k = 45.65;
+        boolean c = false;
+        char z = 'H';
+        String y = "bac ninh city";
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+        System.out.println("c = " + c);
+        System.out.println("z = " + z);
+        System.out.println("y = " + y);
+    }
+}
