86689d02011029650bdd8d93c61c572c7a08b3ee
thidaoC0223g1 <daogiathi231290@gmail.com>
2023-03-22 21:43:16 +0700

    done thuc hanh 1

---
+++ test1/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Date;
+public class Main {
+    public static void main(String[] args) {
+ Date now= new Date();
+         System.out.println(" the time now is " + now );
+
+    }
+}
