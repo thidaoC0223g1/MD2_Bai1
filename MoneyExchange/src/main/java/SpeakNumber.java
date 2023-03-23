212f3b14c8e135626963d06c6df6f4cf68ae7271
thidaoC0223g1 <daogiathi231290@gmail.com>
2023-03-23 21:30:38 +0700

    done bai tap 1

--- MoneyExchange/src/main/java/SpeakNumber.java
+++ MoneyExchange/src/main/java/SpeakNumber.java
@@ -5,63 +5,101 @@ public class SpeakNumber {
         Scanner input = new Scanner(System.in);
         System.out.println("nhap vao so can doc");
         int num = input.nextInt();
-        int tram = num / 100;
-        int chuc2 = num % 100;
-        int chuc=chuc2/10;
-        int dv = chuc2 % 10;
-        String tram1="";
-        String chuc1="";
-        String dv1="";
-
-        if(num>1000){
+        int tram = num / 100, chuc = (num % 100) / 10, dv = (num % 100) % 10;
+        String tram1 = "", chuc1 = "", dv1 = "";
+        if (num > 1000) {
             System.out.println("out of ability");
-        }else if (num>0){
-            switch (tram){
-                case 0: break;
-                case 1:  tram1="one hundred ";break;
-                case 2:  tram1="two hundred ";break;
-                case 3:  tram1="three hundred ";break;
-                case 4:  tram1="four hundred ";break;
-                case 5:  tram1="five hundred ";break;
-                case 6:  tram1="six hundred ";break;
-                case 7:  tram1="seven hundred ";break;
-                case 8:  tram1="eight hundred ";break;
-                case 9:  tram1="nine hundred ";break;
-
+        } else if (num >= 20 || num <= 10) {
+            switch (tram) {
+                case 0:
+                    break;
+                case 1:
+                    tram1 = "one hundred ";
+                    break;
+                case 2:
+                    tram1 = "two hundred ";
+                    break;
+                case 3:
+                    tram1 = "three hundred ";
+                    break;
+                case 4:
+                    tram1 = "four hundred ";
+                    break;
+                case 5:
+                    tram1 = "five hundred ";
+                    break;
+                case 6:
+                    tram1 = "six hundred ";
+                    break;
+                case 7:
+                    tram1 = "seven hundred ";
+                    break;
+                case 8:
+                    tram1 = "eight hundred ";
+                    break;
+                case 9:
+                    tram1 = "nine hundred ";
+                    break;
             }
-            switch (chuc){
-                case 0: break;
-                case 1:  chuc1="ten ";break;
-                case 2:  chuc1="twenty ";break;
-                case 3:  chuc1="thirty ";break;
-                case 4:  chuc1="forty ";break;
-                case 5:  chuc1="fifty ";break;
-                case 6:  chuc1="sixty ";break;
-                case 7:  chuc1="seventy ";break;
-                case 8:  chuc1="eighty ";break;
-                case 9:  chuc1="ninety ";break;
-
+            switch (chuc) {
+                case 0:
+                    break;
+                case 1:
+                    chuc1 = "ten ";
+                    break;
+                case 2:
+                    chuc1 = "twenty ";
+                    break;
+                case 3:
+                    chuc1 = "thirty ";
+                    break;
+                case 4:
+                    chuc1 = "forty ";
+                    break;
+                case 5:
+                    chuc1 = "fifty ";
+                    break;
+                case 6:
+                    chuc1 = "sixty ";
+                    break;
+                case 7:
+                    chuc1 = "seventy ";
+                    break;
+                case 8:
+                    chuc1 = "eighty ";
+                    break;
+                case 9:
+                    chuc1 = "ninety ";
+                    break;
             }
-            switch (dv){
-                case 0:  break;
-                case 1:  dv1="one";break;
-                case 2:  dv1="two ";break;
-                case 3:  dv1="three ";break;
-                case 4:  dv1="four ";break;
-                case 5:  dv1="five ";break;
-                case 6:  dv1="six ";break;
-                case 7:  dv1="seven ";break;
-                case 8:  dv1="eight ";break;
-                case 9:  dv1="nine ";break;
-
+            switch (dv) {
+                case 0 -> dv1 = "zero";
+                case 1 -> dv1 = "one";
+                case 2 -> dv1 = "two ";
+                case 3 -> dv1 = "three ";
+                case 4 -> dv1 = "four ";
+                case 5 -> dv1 = "five ";
+                case 6 -> dv1 = "six ";
+                case 7 -> dv1 = "seven ";
+                case 8 -> dv1 = "eight ";
+                case 9 -> dv1 = "nine ";
             }
-            System.out.println(tram1+chuc1+dv1);
-        }else{
-            System.out.println("zero");
+            System.out.println(tram1 + chuc1 + dv1);
+        } else {
+            switch (num) {
+                case 11 -> System.out.println("eleven");
+                case 12 -> System.out.println("twelve");
+                case 13 -> System.out.println("thirteen");
+                case 14 -> System.out.println("fourteen");
+                case 15 -> System.out.println("fifteen");
+                case 16 -> System.out.println("sixteen");
+                case 17 -> System.out.println("seventeen");
+                case 18 -> System.out.println("eighteen");
+                case 19 -> System.out.println("nineteen");
+            }
         }
+
     }
-
 }
-
 
 
